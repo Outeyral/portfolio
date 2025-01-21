45ddba36c4abfccfc00d6ade1bd7bcceac1119c8
Outeyral <ju.outeyral@hotmail.com>
2025-01-21 13:41:03 -0300

    show more

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -44,9 +44,6 @@ const Navbar = () => {
         </ul>
 
         <div className='flex items-center gap-4'>
-            <button>
-                <Image src={assets.moon_icon} alt="" className='w-6'/>
-            </button>
 
             <a href='#contact' className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact <Image src={assets.arrow_icon} alt="" className='w-3'/></a>
 
@@ -67,7 +64,7 @@ const Navbar = () => {
             <li><a className='font-Ovo' onClick={closeMenu} href='#about'>About me</a></li>
             <li><a className='font-Ovo' onClick={closeMenu} href='#services'>Services</a></li>
             <li><a className='font-Ovo' onClick={closeMenu} href='#work'>My work</a></li>
-            <li><a className='font-Ovo' onClick={closeMenu} href='#contact'>Contact me</a></li>
+            <li><a className='font-Ovo' onClick={closeMenu} href='#contact'>Contact</a></li>
         </ul>
 
       </nav>
